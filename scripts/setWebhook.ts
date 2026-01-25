0d74d9643cd8891a3cb715f9e4320d14ab4d3fef
ElOleksii <letel315@gmail.com>
2026-01-25 13:23:22 +0200

    change webhook

--- scripts/setWebhook.ts
+++ scripts/setWebhook.ts
@@ -4,6 +4,6 @@ import { Bot } from "grammy";
 const bot = new Bot(process.env.BOT_TOKEN!);
 
 (async () => {
-  await bot.api.setWebhook("https://workouts-log-bot.vercel.app/");
+  await bot.api.setWebhook("https://workouts-log-bot.vercel.app/api/webhook");
   console.log("Webhook set!");
 })();
